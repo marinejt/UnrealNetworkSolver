2c696605f2ff168358e11a2bb7541e069798ea41
OnEdgePat <tremblay.patrick@gmail.com>
2014-11-13 21:12:54 -0500

    PathWrite done! Beau p'tit résultat.

--- gti310/tp3/writer/PathWriter.java
+++ gti310/tp3/writer/PathWriter.java
@@ -1,26 +1,30 @@
 package gti310.tp3.writer;
 
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
-public class PathWriter implements Writer {
+public class PathWriter implements Writer<Object> {
 
+	@SuppressWarnings("unchecked")
 	public void write(String filename, Object output) {
+		ArrayList<int[]> sol = new ArrayList<int[]>();
+		sol = (ArrayList<int[]>) output;
+		
 		try {
-	         // create a new file with an ObjectOutputStream
-	         FileOutputStream fos = new FileOutputStream(filename);
-	         ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-	         // write something in the file
-	         oos.writeObject(output.toString());
-
-	         // close the stream
-	         oos.close();
-
-	      } catch (Exception ex) {
-	         ex.printStackTrace();
-	      }
-
+			PrintStream fileStream = new PrintStream(new File(filename));
+			
+			for (int[] item : sol) {   
+				for (int j = 0; j < item.length; j++){
+					fileStream.print(item[j] + " ");
+				}
+				fileStream.print("\n");
+			}
+		   fileStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
